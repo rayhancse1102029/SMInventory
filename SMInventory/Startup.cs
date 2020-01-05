b66f3334c16c6481fe4d0d11ba13c0d61e58a3cc
rayhancse15git <abu@baiust.edu.bd>
2020-01-05 22:40:16 +0600

    gh

--- SMInventory/Startup.cs
+++ SMInventory/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace SMInventory
 {
@@ -55,6 +56,10 @@ namespace SMInventory
 
             app.UseMvc(routes =>
             {
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area=exits}{controller=Home}/{action=Index}/{id?}");
+
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
